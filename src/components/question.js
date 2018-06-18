01b1f2bd9493b3c05e4e7edc3295270197c37bb3
Dean Macaulay <dean@magpies.net>
2018-06-19 07:13:22 +1000

    Added first tab

--- src/components/question.js
+++ src/components/question.js
@@ -1,12 +1,14 @@
+import { Button, Card, CardText, CardTitle, Col, Nav, NavItem, NavLink, Row, TabContent, TabPane } from 'reactstrap';
 import React, { Component } from 'react';
 
+import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { formatQuestion } from '../utils/_DATA';
 
 class Question extends Component {
     render() {
         const question = this.props.question
-        console.log("!!!",question)
+
         if (question === null) {
             return <p> This question does not exist</p>
         }
@@ -23,24 +25,33 @@ class Question extends Component {
 
         return (
             <div>
-                <h5>{id}</h5>
-                <p>{author}</p>
-                <p>{timestamp}</p>
-                <p>{optionOne.text}: {optionOne.votes.length}</p>
-                <p>{optionTwo.text}: {optionTwo.votes.length}</p>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            // className={classNames({ active: this.state.activeTab === '1' })}
+                            onClick={() => { this.toggle('1'); }}>
+                            Unanswered questions
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                    <h5>{id}</h5>
+                    <p>{author}</p>
+                    <p>{timestamp}</p>
+                    <p>{optionOne.text}: {optionOne.votes.length}</p>
+                    <p>{optionTwo.text}: {optionTwo.votes.length}</p>
             </div>
-        )
-    }
-}
-
+                )
+            }
+        }
+        
 function mapStateToProps({authedUser, users, questions},{id}){
     const question = questions[id]
     return {
-        authedUser,
-        question: question
-        // ? formatQuestion(question.optionOne,question.optionTwo, question.author)
-        // : null
-    }
-}
-
+                    authedUser,
+                question: question
+                // ? formatQuestion(question.optionOne,question.optionTwo, question.author)
+                // : null
+            }
+        }
+        
 export default connect(mapStateToProps)(Question);
